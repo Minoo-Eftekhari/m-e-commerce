72e5a90e25f9f70f6c23c4fb951291591ab4a543
Minoo Eftekhari <minoo.eftekhari5448@gmail.com>
2024-05-02 13:48:20 +0330

    Style the active link in the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,14 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { ImWink2 } from "react-icons/im";
+import classnames from "classnames";
 
 const NavBar = () => {
+  const currentPath = usePathname();
+  console.log(currentPath);
   const links = [
     { label: "Cart", href: "/cart" },
     { label: "Sign In", href: "/signin" },
@@ -18,10 +24,14 @@ const NavBar = () => {
           {links.map((link) => (
             <Link
               key={link.href}
-              className="text-zinc-500 hover:text-zinc-800 transition-colors"
+              className={classnames({
+                "!text-zinc-900": link.href === currentPath,
+                "!text-zinc-500": link.href !== currentPath,
+                "hover:text-zinc-800 transition-colors": true,
+              })}
               href={link.href}
             >
-            {link.label}
+              {link.label}
             </Link>
           ))}
         </ul>
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,3 @@
-
-
 export default function Home() {
-  return (
-   <div>Hello World</div>
-
-  )
+  return <h2 className="text-2xl py-2">Latest Products</h2>;
 }
